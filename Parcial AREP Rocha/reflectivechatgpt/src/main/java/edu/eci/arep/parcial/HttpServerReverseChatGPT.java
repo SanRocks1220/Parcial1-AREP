47746aea75770221a625684850e81b0f42f48093
unknown <santiago.rocha-c@labinfo.is.escuelaing.edu.co>
2023-09-12 14:07:10 -0500

    Server con múltiples peticiones funcionando

--- Parcial AREP Rocha/reflectivechatgpt/src/main/java/edu/eci/arep/parcial/HttpServerReverseChatGPT.java
+++ Parcial AREP Rocha/reflectivechatgpt/src/main/java/edu/eci/arep/parcial/HttpServerReverseChatGPT.java
@@ -15,90 +15,99 @@ public class HttpServerReverseChatGPT {
 
         Socket clientSocket = null;
         Boolean running = true;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-        PrintWriter out = new PrintWriter(
-                clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Recibí: " + inputLine);
-            if (!in.ready()) {
-                break;
+        while (running) {
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
             }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            System.out.println("A");
+            outputLine = htmlPage();
+            // outputLine += outputLineResponse();
+
+            out.println(outputLine);
+            out.close();
+            in.close();
+
+            System.out.println("B");
         }
 
-        outputLine = htmlPage();
-        //outputLine = outputLineResponse();
-        
-        out.println(outputLine);
-        out.close();
-        in.close();
         clientSocket.close();
         serverSocket.close();
     }
 
     private static String htmlPage() {
-        String response = "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "    <head>\n" +
-                "        <title>Form Example</title>\n" +
-                "        <meta charset=\"UTF-8\">\n" +
-                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    </head>\n" +
-                "    <body>\n" +
-                "        <h1>Form with GET</h1>\n" +
-                "        <form action=\"/hello\">\n" +
-                "            <label for=\"name\">Name:</label><br>\n" +
-                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
-                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
-                "        </form> \n" +
-                "        <div id=\"getrespmsg\"></div>\n" +
-                "\n" +
-                "        <script>\n" +
-                "            function loadGetMsg() {\n" +
-                "                let nameVar = document.getElementById(\"name\").value;\n" +
-                "                const xhttp = new XMLHttpRequest();\n" +
-                "                xhttp.onload = function() {\n" +
-                "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
-                "                    this.responseText;\n" +
-                "                }\n" +
-                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
-                "                xhttp.send();\n" +
-                "            }\n" +
-                "        </script>\n" +
-                "\n" +
-                "        <h1>Form with POST</h1>\n" +
-                "        <form action=\"/hellopost\">\n" +
+        String response = "GET / HTTP/1.1\n\r" +
+                "200 OK\n\r" +
+                "\n\r" +
+                "<!DOCTYPE html>\n\r" +
+                "<html>\n\r" +
+                "    <head>\n\r" +
+                "        <title>Form Example</title>\n\r" +
+                "        <meta charset=\"UTF-8\">\n\r" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\r" +
+                "    </head>\n\r" +
+                "    <body>\n\r" +
+                "        <h1>Form with GET</h1>\n\r" +
+                "        <form action=\"/hello\">\n\r" +
+                "            <label for=\"name\">Name:</label><br>\n\r" +
+                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n\r" +
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n\r" +
+                "        </form> \n\r" +
+                "        <div id=\"getrespmsg\"></div>\n\r" +
+                "\n\r" +
+                "        <script>\n\r" +
+                "            function loadGetMsg() {\n\r" +
+                "                let nameVar = document.getElementById(\"name\").value;\n\r" +
+                "                const xhttp = new XMLHttpRequest();\n\r" +
+                "                xhttp.onload = function() {\n\r" +
+                "                    document.getElementById(\"getrespmsg\").innerHTML =\n\r" +
+                "                    this.responseText;\n\r" +
+                "                }\n\r" +
+                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n\r" +
+                "                xhttp.send();\n\r" +
+                "            }\n\r" +
+                "        </script>\n\r" +
+                "\n\r" +
+                "        <h1>Form with POST</h1>\n\r" +
+                "        <form action=\"/hellopost\">\n\r" +
                 "            <label for=\"postname\">Name:</label><br>\n" +
-                "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
-                "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
-                "        </form>\n" +
-                "        \n" +
-                "        <div id=\"postrespmsg\"></div>\n" +
-                "        \n" +
-                "        <script>\n" +
-                "            function loadPostMsg(name){\n" +
-                "                let url = \"/hellopost?name=\" + name.value;\n" +
-                "\n" +
-                "                fetch (url, {method: 'POST'})\n" +
-                "                    .then(x => x.text())\n" +
-                "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
-                "            }\n" +
-                "        </script>\n" +
-                "    </body>\n" +
+                "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n\r" +
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n\r" +
+                "        </form>\n\r" +
+                "        \n\r" +
+                "        <div id=\"postrespmsg\"></div>\n\r" +
+                "        \n\r" +
+                "        <script>\n\r" +
+                "            function loadPostMsg(name){\n\r" +
+                "                let url = \"/hellopost?name=\" + name.value;\n\r" +
+                "\n\r" +
+                "                fetch (url, {method: 'POST'})\n\r" +
+                "                    .then(x => x.text())\n\r" +
+                "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n\r" +
+                "            }\n\r" +
+                "        </script>\n\r" +
+                "    </body>\n\r" +
                 "</html>";
 
         return response;
     }
 
-    private static String outputLineResponse(){
+    private static String outputLineResponse() {
         String outputLine = "<!DOCTYPE html>" +
                 "<html>" +
                 "<head>" +
